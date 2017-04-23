b0639067e82b4047b9f1e4c76ac134ea7738853d
Marcella Pantarotto <marcellapantarotto@gmail.com>
2017-04-23 19:24:07 -0300

    Fixing the non printig of the results problem (after the switch of the for loop).

--- b16-lab/test-ball.cpp
+++ b16-lab/test-ball.cpp
@@ -5,11 +5,21 @@
 
 #include "ball.h"
 
+void run(Simulation & s, double dt){
+  	for(int i=0; i<100; ++i){
+  		s.step(dt); s.display();
+  	}
+}
+
 int main(int argc, char** argv)
 {
   Ball ball ;
   const double dt = 1.0/30 ;
   
+  // Adding run call.
+  run(ball, dt);
+
+  // Removing for loop
   /*for (int i = 0 ; i < 100 ; ++i) {
     ball.step(dt) ;
     ball.display() ;
@@ -18,9 +28,3 @@ int main(int argc, char** argv)
   return 0 ;
 }
 
-
-void run(Simulation & s, double dt){
-  	for(int i=0; i<100; ++i){
-  		s.step(dt); s.display();
-  	}
-}
