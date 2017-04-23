fb7d0ebecc0b2d7a93f446f6666bd509a26f13fb
Marcella Pantarotto <marcellapantarotto@gmail.com>
2017-04-23 19:04:21 -0300

    Switching the for loop on 'test-ball.cpp' file. No print of the result with this change.

--- b16-lab/test-ball.cpp
+++ b16-lab/test-ball.cpp
@@ -9,9 +9,18 @@ int main(int argc, char** argv)
 {
   Ball ball ;
   const double dt = 1.0/30 ;
-  for (int i = 0 ; i < 100 ; ++i) {
+  
+  /*for (int i = 0 ; i < 100 ; ++i) {
     ball.step(dt) ;
     ball.display() ;
-  }
+  }*/
+
   return 0 ;
+}
+
+
+void run(Simulation & s, double dt){
+  	for(int i=0; i<100; ++i){
+  		s.step(dt); s.display();
+  	}
 }
